6671b6afda46aec130380be9d0025b45bd6a822c
saikumarsunkari <saikumarsunkari7277@gmail.com>
2024-04-20 19:37:10 +0530

    Changes_on_javascript

---
+++ loops.js
@@ -0,0 +1,94 @@
+// var a = 1;
+// while (a <= 10) {
+//     console.log(a);
+//     a++
+// }
+
+
+// var b = 5;
+// while (b <= 10) {
+//     console.log(b);
+//     b++
+// }
+
+// var c = +prompt();
+// while (c <= 10) {
+//     console.log(c);
+//     c++
+// }
+
+
+// -45 to -25
+
+// var d = +prompt()
+// while (d <= -25) {
+//     console.log(d);
+//     d++
+// }
+
+
+// 35-45
+// var e = +prompt()
+// while (e <= 45) {
+//     console.log(e)
+//     e++
+// }
+
+
+// 21 to 57
+// var f = +prompt()
+// while (f <= 57) {
+//     console.log(f)
+//     f++
+// }
+
+
+// 10 to 1
+
+
+// var g = +prompt()
+// while (g >= 1) {
+//     console.log(g)
+//     g++
+// }
+
+
+// 25-10
+
+// var d = +prompt()
+// while (d >= 25) {
+//     console.log(d)
+//     d++
+// }
+
+
+
+var a=4
+i=1
+while(i<=10)
+{
+    console.log(a*i)
+    i++
+}
+
+
+
+var b=6
+i=1
+while(i<11){
+    console.log(b*i)
+    i++
+}
+
+
+var a=1;
+while(a<=10){
+    console.log(a*a)
+    a++
+}
+
+var b=1
+while(b<=10){
+    console.log(b**3)
+    b++
+}
---
+++ loops2.js
@@ -0,0 +1,295 @@
+// var a;
+// var a=+prompt()
+// while(a<=10){
+// 	if(a%2===0){
+// 		console.log(a)
+// 	}
+// 	a++
+// }
+
+// var b;
+// var b=+prompt()
+// while(b<=25){
+// 	if (b%2===1) {
+// 		console.log(b)
+
+// 	}
+// 	b++
+// }
+
+
+// var c;
+// var c=+prompt()
+// while(c<=56){
+// 	if(c%2===0){
+// 		console.log(c)
+// 	}
+// 	c++
+// }
+
+
+// -12 to -48
+
+
+// var d;
+// var d = +prompt()
+// while (d <= -12) {
+// 	if (d % 2 !== 0) {
+// 		console.log(d)
+// 	}
+// 	d++
+// }
+
+
+
+// -50 to 8 even number;
+
+//  var a;
+//  var a=+prompt()
+// while(a<=8){
+// 	if(a%2===0){
+// 		console.log(a)
+// 	}
+// 	a++
+// }
+
+
+// 20 to 1 even number
+
+
+// var c;
+// var c=+prompt()
+// while(c>=1){
+// 	if (c%2===0) {
+// 		console.log(c)
+// 	}
+// 	c--
+// }
+
+// odd 46 to 23
+// var a;
+// var a=+prompt()
+// while (a>=23){
+// 	if (a%2!==0) {
+// 		console.log(a)
+// 	}
+// 	a--
+// }
+
+// var e;
+// var e=+prompt()
+// while(e>=-4){
+// 	if (e%2===0) {
+// 		console.log(e)
+// 	}
+// 	e--
+// }
+
+
+// var w=+prompt()
+// while(w>=-36){
+// 	if(w%2!==0){
+// 		console.log(w)
+// 	}
+// 	w--
+// }
+
+// var a=1
+// while(a<=24){
+// 	if (24%a==0) {
+// 		console.log(a)
+// 	}
+// 	a++
+// }
+
+
+
+// var a=5
+// var i=1
+// var max=-Infinity
+// while(i<=a)
+// {
+// 	var b=+prompt()
+// 	if (b>max) {
+// 		max=b
+// 	}
+// 	i++
+// }
+// console.log(max)
+
+
+// var a=5
+// var i=1
+// var min=Infinity
+// while(i<=a){ 
+// 	var e=+prompt()
+// 	if (e<min) {
+// 		min=e
+// 	}
+// 	i++
+// }
+// console.log(min)
+
+
+
+
+
+// var b = +prompt()
+// var i = 1
+// while (i <= b) {
+// 	a = +prompt()
+// 	if (a % 2 == 0) {
+
+// 		console.log(a)
+// 	}
+// 	i++
+// }
+
+
+// given number is 154256 print even number 
+
+
+// var a;
+// var i = 1;
+// while (i <= 6) {
+// 	a = +prompt()
+// 	if (a % 2 == 0) {
+// 		console.log(a)
+// 	}
+// 	i++
+
+// }
+
+
+
+// given number is 1542763 print odd number
+
+
+
+// var a;
+// var i = 1
+// while (i <= 7) {
+// 	a = +prompt()
+// 	if (a % 2 == 1) {
+// 		console.log(a)
+// 	}
+// 	i++
+// }
+
+
+// var a = 123
+// var i = 1;
+// while (i <= 2) {
+// 	rem = (a % 10)
+// 	a = parseInt(a / 10)
+// 	i++
+// }
+// console.log(rem)
+
+
+
+
+// var a = 1436
+// var i = 1
+// while (i <= 3) {
+// 	rem = (a % 10)
+// 	a = parseInt(a / 10)
+// 	i++
+// }
+// console.log(rem)
+
+
+// var a = +prompt()
+// var i = 1
+// while (i <= 4) {
+// 	rem = (a % 10)
+// 	a = parseInt(a / 10)
+// 	i++
+// }
+// console.log(rem)
+
+
+
+// var a = +prompt()
+// var i = 1
+// while (i <= 5) {
+// 	rem = (a % 10)
+// 	a = parseInt(a / 10)
+// 	i++
+// }
+// console.log(rem)
+
+
+
+
+// var i = 1;
+// sum = 0;
+// while (i <= 10) {
+// 	// a = +prompt()
+// 	sum = (sum + i);
+// 	i++;
+// }
+// 	console.log(sum)
+
+
+
+// var a=10;
+// var i=1;
+// sum=0;
+// while(i<=10){
+// 	sum=(sum+i);
+// 	i++
+// }
+// console.log(sum/10)
+
+
+// var a;
+// var i=1;
+// while(i<=10){
+// 	if (a%2==0)
+// 		a=+prompt()
+// 	 {
+// 		console.log(i)
+// 	}
+// 	i++
+// }
+
+
+
+//  
+
+
+
+// var a=7;
+// var i=1;
+// var sum=0;
+// while(i<=7){
+// 	if(i%2==1){
+// 		sum=sum+i
+// 	}
+// 	i++
+// }
+// console.log(sum)
+
+
+
+var a=5;
+var i=1;
+var product=1;
+while(i<=5){
+	product=a*i;
+	i++
+}
+console.log(product)
+
+var a=7;
+var i=1;
+var product=1;
+while(i<=a){
+	if (i%2==1) {
+	product=product*i
+}
+	i++
+
+}
+console.log(product)  
